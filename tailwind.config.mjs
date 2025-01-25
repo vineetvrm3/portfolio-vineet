c47b6c2dd99070269310d837fcd41fd9427f84c9
vineet <vineet@peregrine-it.com>
2025-01-25 20:18:13 +0530

    navbar headers and about section added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f'
       },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      },
+      boxShadow: {
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff'
+      }
     },
   },
   plugins: [],
